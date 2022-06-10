33d99c8fdf3cab807a4b049a7a65cb41f60cbd3a
AidanStrong <strongaidan13@gmail.com>
2022-06-10 15:53:02 +0100

    configured xrdp, cmd line install of burp suite and install to /root

--- build.sh
+++ build.sh
@@ -1,24 +1,36 @@
-
 #!/bin/bash
 
+echo "Please execute script as root"
+
+apt-get update -y
+apt-get upgrade -y
+
+mkdir /root/tools
+
+echo "Configuring xrdp"
+apt-get install xrdp -y
+apt-get install xfce4 -y
+echo xfce4-session >~/.xsession
+service xrdp restart
+
 echo "Downloading Burp Suite - Professional"
-wget "https://portswigger-cdn.net/burp/releases/download?product=pro&version=2022.3.9&type=Linux" -O burpsuite_pro.sh
-chmod +x burpsuite_pro.sh
-printf '\n' |./burpsuite_pro.sh -c
+wget "https://portswigger-cdn.net/burp/releases/download?product=pro&version=2022.3.9&type=Linux" -O /root/tools/burpsuite_pro.sh
+chmod +x /root/tools/burpsuite_pro.sh
+printf '\n\n\n\n\n\n' | /root/tools/burpsuite_pro.sh -c
 echo "Complete!"
 
 echo "Downloading Burp Suite - Community"
-wget "https://portswigger-cdn.net/burp/releases/download?product=community&version=2022.3.9&type=Linux" -O burpsuite_community.sh
-chmod +x burpsuite_community.sh
-printf '\n' |./burpsuite_community.sh -c
+wget "https://portswigger-cdn.net/burp/releases/download?product=community&version=2022.3.9&type=Linux" -O /root/tools/burpsuite_community.sh
+chmod +x /root/tools/burpsuite_community.sh
+printf '\n\n\n\n' | /root/tools/burpsuite_community.sh -c
 echo "Complete!"
 
 echo "Downloading MobSF"
-git clone "https://github.com/MobSF/Mobile-Security-Framework-MobSF.git"
+git clone "https://github.com/MobSF/Mobile-Security-Framework-MobSF.git" /root/tools/Mobile-Security-Framework-MobSF
 echo "Complete!"
 
 echo "Downloading Android Studio"
-wget https://redirector.gvt1.com/edgedl/android/studio/ide-zips/2021.2.1.15/android-studio-2021.2.1.15-linux.tar.gz -O Android-Studio.tar.gz
+wget https://redirector.gvt1.com/edgedl/android/studio/ide-zips/2021.2.1.15/android-studio-2021.2.1.15-linux.tar.gz -O /root/tools/Android-Studio.tar.gz
 tar -xvzf Android-Studio.tar.gz
 echo "Complete!"
 
