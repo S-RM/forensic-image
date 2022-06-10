ae44c942ef976e87c84dc1335d96ca9a28f4de74
AidanStrong <strongaidan13@gmail.com>
2022-06-10 14:46:00 +0100

    updated script

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
+
 #!/bin/bash
 
 echo "Downloading Burp Suite - Professional"
 wget "https://portswigger-cdn.net/burp/releases/download?product=pro&version=2022.3.9&type=Linux" -O burpsuite_pro.sh
 chmod +x burpsuite_pro.sh
-./burpsuite_pro.sh -c -y
-
-
+printf '\n' |./burpsuite_pro.sh -c
 echo "Complete!"
 
 echo "Downloading Burp Suite - Community"
 wget "https://portswigger-cdn.net/burp/releases/download?product=community&version=2022.3.9&type=Linux" -O burpsuite_community.sh
 chmod +x burpsuite_community.sh
+printf '\n' |./burpsuite_community.sh -c
 echo "Complete!"
 
 echo "Downloading MobSF"
@@ -20,9 +20,8 @@ echo "Complete!"
 echo "Downloading Android Studio"
 wget https://redirector.gvt1.com/edgedl/android/studio/ide-zips/2021.2.1.15/android-studio-2021.2.1.15-linux.tar.gz -O Android-Studio.tar.gz
 tar -xvzf Android-Studio.tar.gz
-
 echo "Complete!"
 
-
-
 echo "Build Script finished, preparing to be captured into golden image..."
+
+#waagent -deprovision -force
